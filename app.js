782c4c52d8ba9e18ca2ff22947a7cc1023920e70
Madlen <madlensarkisian@gmail.com>
2020-03-12 19:14:13 +0200

    Created function which matches users by first interest

--- app.js
+++ app.js
@@ -32,6 +32,42 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, async (error, d
             })
         });
     })
+    
+    // Match user with common interests
+    const matchByInterest = (user, interest) => {
+        db.collection('users').find(
+            {
+                interests: {
+                    $elemMatch: {
+                        $eq: interest
+                    }
+                }
+            }
+        ).toArray((error, result) => {
+            if (error) {
+                throw error
+            }
+            
+            result.forEach(match => {
+                if (user.name !== match.name) {
+                    console.log(user.name + ' matched with ' + match.name)                    
+                    console.log('They are both interest in ' + interest)
+                    console.log()
+                }
+            });
+        })
+    }
+
+    db.collection('users').find({}).toArray((error, users) => {
+        if (error) {
+            throw error
+        }
+
+        users.forEach(user => {
+            // Match current user with someone who has the 1st interest as user
+            matchByInterest(user, user.interests[0])
+        });
+    })
 
     setTimeout(() => {
         database.close()
