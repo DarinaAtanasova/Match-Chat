50aec200b851a3569aaffc79f0ec1d00c997fbd8
Madlen <madlensarkisian@gmail.com>
2020-03-22 18:37:53 +0200

    Added test for birthday matching

---
+++ tests/birthday.test.js
@@ -0,0 +1,48 @@
+const {MongoClient} = require('mongodb');
+
+ObjectID = require('mongodb').ObjectID
+
+describe('match birthday', () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect('mongodb://127.0.0.1:27017');
+        db = await connection.db("match-and-chat");
+    });
+
+    afterAll(async () => {
+        await connection.close();
+        await db.close();
+    });
+
+    it('test if user have matches born on the same date', async () => {
+
+        const matchByBirthday = (user, callback) => {  
+            db.collection('users').aggregate([
+                {
+                    $match: {
+                        birthday: user.birthday
+                    }
+                }
+            ]).toArray().then(allMatches => {
+                allMatches.forEach(matchedUser => {
+                    if (!matchedUser._id.equals(user._id)) {
+                        callback(undefined, matchedUser)
+                    }
+                });
+            })
+        }
+
+        user_Madi = {
+            _id: ObjectID("5e67ba85237c1e40a34f7f93"),
+            name: 'Madi',
+            birthday: new Date("2002-08-25"),
+            interests: ['books', 'movies', 'tv']
+        }
+
+        matchByBirthday(user_Madi, (error, result) => {
+            expect(result.name).toBe('Nick')
+        })
+    });
+});
