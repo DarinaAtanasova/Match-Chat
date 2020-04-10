4a3f74dc60b7edcc182cc022c2c5d62105393781
Madlen <madlensarkisian@gmail.com>
2020-04-10 23:20:37 +0300

    [Update] Removed user schema

--- database/database.js
+++ database/database.js
@@ -1,4 +1,4 @@
-const connectionURL = 'mongodb://localhost:27017'
+const connectionURL = 'mongodb://localhost:27017/match-and-chat'
 
 var mongoose = require('mongoose');
 mongoose.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -8,19 +8,3 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('We are connected!')
 });
-
-var Schema = mongoose.Schema;
-
-var userSchema = new Schema({
-    email:  String, 
-    password: String,
-    username: String,
-    birthday: Date,
-    interests: [{
-        type: String
-    }]
-});
-
-var User = mongoose.model('User', userSchema);
-
-db.close();
