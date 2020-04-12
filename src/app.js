464e1b5cbc7b6726b1b29475737c00d221836a87
Madlen <madlensarkisian@gmail.com>
2020-04-12 21:59:55 +0300

    [New] Add login/logout session

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const path = require('path')
+const session = require('express-session');
+const path = require('path');
 
-require('../database/database')
+require('../database/database');
 
 var User = require('../database/models/user.js');
 
@@ -19,16 +20,36 @@ app.use(express.json());
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'match'
+}))
+
 // Setup handlebars engine
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const { userId } = req.session;
+    if (userId) {
+        res.render('index', { id: userId });
+    }
+    else
+    {
+        res.render('index');
+    }
 })
 
 app.get('/about',(req, res) => {
-    res.render('about');
+    const { userId } = req.session;
+    if (userId) {
+        res.render('about', { id: userId });
+    }
+    else
+    {
+        res.render('about');
+    }
 })
 
 app.get('/signup', (req, res) => {
@@ -41,6 +62,7 @@ app.post('/signup', async (req, res) => {
     try {
         await user.save();
         res.status(201);
+        req.session.userId = user._id;
         res.redirect('/profile');
     } catch (e) {
         res.status(400).send(e);
@@ -55,14 +77,42 @@ app.post('/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
         console.log(user.username + " has succesfully logged in!");
+        req.session.userId = user._id;
         res.redirect('/profile');
     } catch (e) {
         res.status(400).send();
     }
 })
 
-app.get('/profile', (req, res) => {
-    res.render('profile')
+app.get('/profile', async (req, res) => {
+    const { userId } = req.session;
+    var user = await User.findById(userId);
+    
+    if (!user) {
+        res.redirect('/');
+    }
+    else
+    {
+        res.render('profile', {
+            username: user.username,
+            email: user.email,
+            birthday: user.birthday
+        });
+    }
+    
+})
+
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            res.redirect('/');
+        }
+        
+        res.clearCookie();
+        res.redirect('/login');
+        console.log("User has succesfully logged out.");
+
+    })
 })
 
 app.listen(PORT, () => console.log('Server started on port ' + PORT));
