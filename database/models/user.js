453a6cd8a7e92f355121532a57895efee1c144b2
Madlen <madlensarkisian@gmail.com>
2020-04-12 12:13:42 +0300

    [New] Add static method for user model

--- database/models/user.js
+++ database/models/user.js
@@ -13,7 +13,23 @@ var userSchema = new Schema({
     }]
 });
 
-// Setup Middleware
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email: email })
+
+    if (!user) {
+        throw new Error("Unable to login!");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error("Unable to login!");
+    }
+
+    return user;
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     
     if (this.isModified('password')) {
